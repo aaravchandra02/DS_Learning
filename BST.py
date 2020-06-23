80dc4191b30ced4e334bd8c3a3a88f1db757b94b
Aarav Chandra <aarav@Aaravs-MBP.home>
2020-06-23 14:49:42 -0700

    Added min, max and sum of BST

--- BST.py
+++ BST.py
@@ -1,6 +1,8 @@
 """
 My implementation of a Binary Search Tree with its 3 different types of traversals. 
 """
+# To find System properties
+import sys
 
 
 class BSTNode:
@@ -10,22 +12,28 @@ class BSTNode:
         self.right = None
 
     def addChild(self, child):
+
         # Dont do anything if value already exist(set is implemented using BST).
         if (child == self.val):
             return
+
         if (child < self.val):
             # add data in left subtree
             # Check if you are the leaf
+
             if self.left:  # Enters if not a leaf node.
                 # Recursive method makes it very easy.
                 self.left.addChild(child)
+
             else:  # Enters if its a leaf node.
                 # Adds a new BSTNode with 'child' as value.
                 self.left = BSTNode(child)
+
         else:
             # add data in right subtree
             if self.right:  # Enters if not a leaf node.
                 self.right.addChild(child)
+
             else:  # Enters if its a leaf node.
                 # Adds a new BSTNode with 'child' as value.
                 self.right = BSTNode(child)
@@ -84,6 +92,34 @@ class BSTNode:
 
         return elements
 
+    def findMin(self):
+        # First element with no left is the smallest element in the BST.
+        if not self.left:
+            return self.val
+        return self.left.findMin()
+
+    def findMax(self):
+        # First element with no right is the largest element in the BST.
+        if not self.right:
+            return self.val
+        return self.right.findMax()
+
+    def calculateSum(self):
+        # Traversed and added every element and finally returned it.
+        summation = 0
+        if self.left:
+            summation += self.left.calculateSum()
+        summation += self.val
+        if self.right:
+            summation += self.right.calculateSum()
+        return summation
+        """
+        def calculate_sum(self):
+        left_sum = self.left.calculate_sum() if self.left else 0
+        right_sum = self.right.calculate_sum() if self.right else 0
+        return self.data + left_sum + right_sum
+        """
+
 
 def buildTree(nums):  # Helper Fn to build a BST from a list of numbers
     root = BSTNode(nums[0])
@@ -93,8 +129,11 @@ def buildTree(nums):  # Helper Fn to build a BST from a list of numbers
 
 
 if __name__ == '__main__':  # main() Fn
-    nums = [21, 41, 1, 20, 9, 12, 85, 12]
+    nums = [21, 41, 1, 20, 9, 12, 85, 17]
     r = buildTree(nums)
     print(f"\n In-order Traversal Results: {r.inorder_recursive()}")
     print(f"\n Pre-order Traversal Results: {r.preorder_recursive()}")
-    print(f"\n Post-order Traversal Results: {r.postorder_recursive()}\n")
+    print(f"\n Post-order Traversal Results: {r.postorder_recursive()}")
+    print(f"\n Minimum Element in the Tree: {r.findMin()}")
+    print(f"\n Maximum Element in the Tree: {r.findMax()}")
+    print(f"\n Summation of all elements: {r.calculateSum()}\n")
